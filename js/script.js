75fd38b928b867dda43a80669f88521f00f9555b
Cailey <143413461+cailey-t@users.noreply.github.com>
2023-09-18 12:42:36 -0500

    Added random image generator

--- js/script.js
+++ js/script.js
@@ -3,10 +3,33 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function(){
+//Pull Data from Lorem Picsum API
+const getImage = async function() {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+
+//Select Random Image from API
+const selectRandomImage = function(images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+//Display Random Image
+const displayImage = function(randomImage){
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+//Click button
+button.addEventListener("click", function() {
+    getImage();
+});
